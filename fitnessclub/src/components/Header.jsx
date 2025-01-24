8baeae6629bb32aca5efcb8d2e293a0921d13bad
pradip <chaudharypradip678@gmail.com>
2025-01-25 01:12:57 +0545

    feat:header layout

---
+++ fitnessclub/src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header(){
+    return(
+        <header className="bg-gray-100 py-2">
+            <div className="container mx-auto">
+                <h1 className="text-3xl font-bold">Header </h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
