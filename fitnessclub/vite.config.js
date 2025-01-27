ade6719c3474aa8ad4a73cc1473753e1b31f304a
pradip <chaudharypradip678@gmail.com>
2025-01-27 19:25:58 +0545

    feat: fixed the deploy bug

--- fitnessclub/vite.config.js
+++ fitnessclub/vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "slick-carousel/slick/slick.css": require.resolve("slick-carousel/slick/slick.css"),
-      "slick-carousel/slick/slick-theme.css": require.resolve("slick-carousel/slick/slick-theme.css")
+      "slick-carousel/slick/slick.css": path.resolve(
+        "node_modules/slick-carousel/slick/slick.css"
+      ),
+      "slick-carousel/slick/slick-theme.css": path.resolve(
+        "node_modules/slick-carousel/slick/slick-theme.css"
+      )
     }
   }
 })
